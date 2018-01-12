fb0de70a1422a8a15e0c2c601b47afcdf59b7282
ach <ignaciomenchaca@gmail.com>
2018-01-12 00:47:43 -0300

    Section 5, Lecture 63. Binding to Custom Events. Se quiere informar al componente padre (app-component) que se ha creado un server nuevo desde app-cockpit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,25 @@ export class AppComponent {
     type: 'server',
     name: 'Testserver',
     content: 'Just a Test!'
-  }];  
+  }];
+  
+  // Los siguientes dos métodos serán ejecutados después de que los botones del componente cockpit se hayan clickeado
+
+  // El metodo espera recibir los datos agregados al servidor
+  // En este caso el parametro que espera es un objeto
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(bluePrintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent
+    });
+  }
 }
