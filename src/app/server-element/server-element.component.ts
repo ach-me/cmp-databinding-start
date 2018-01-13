49e6e37a8d8ed46da4635492362823c3f40d00d4
ach <ignaciomenchaca@gmail.com>
2018-01-12 22:59:31 -0300

    Sección 5, clase 67. More on View Encapsulation. Se agregó un comentario que muestra cómo desencapsular un componente

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ViewEncapsulation } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
+  // Deshabilitar la encapsulacion de los componentes. Permite usar en toda la aplcacion los estilos aplicados en este componente
+  // encapsulation: ViewEncapsulation.None
 })
 export class ServerElementComponent implements OnInit {
   // Para permitir que un componente padre pueda bindear esta propiedad, es necesario agregar un Decorator. @Input()
